33c001edec40e83f43cf9efb6c9efc8374c06591
Samuel Bachmann <samuel.bachmann@gmail.com>
2022-12-13 12:00:12 +0100

    add CrowdSec, use traefik yml file instead of toml

---
+++ crowdsec/setup.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+DATA_DIR="$HOME/docker-data/crowdsec"
+
+mkdir -p "${DATA_DIR}/conf"
+mkdir -p "${DATA_DIR}/data"
--- nextcloud/setup.sh
+++ nextcloud/setup.sh
@@ -4,6 +4,5 @@ DATA_DIR="$HOME/docker-data/nextcloud"
 
 mkdir -p "${DATA_DIR}/mariadb"
 mkdir -p "${DATA_DIR}/redis"
-mkdir -p "${DATA_DIR}/html"
-mkdir -p "${DATA_DIR}/config"
-mkdir -p "${DATA_DIR}/apps"
+mkdir -p "${DATA_DIR}/app"
+mkdir -p "${DATA_DIR}/data"
